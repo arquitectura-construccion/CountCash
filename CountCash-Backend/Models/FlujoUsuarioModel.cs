1dbd72031346ea4b1dfaddd06ad55b578f44644d
Leand3k <theleand3000@gmail.com>
2021-10-14 13:40:53 -0400

    FlujoUsuarioModel creado.

---
+++ CountCash-Backend/Models/FlujoUsuarioModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CountCash_Backend.Models
+{
+    public class FlujoUsuarioModel
+    {
+        public int FlujoUsuarioID { get; set; }
+        public int UsuarioID { get; set; }
+        public int TipoMontoID { get; set; }
+        public decimal Monto { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public int TipoFlujoID { get; set; }
+
+        public DateTime FechaRealizacion { get; set; }
+    }
+}
